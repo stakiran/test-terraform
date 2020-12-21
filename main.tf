86eef7d1cebe14ab65637253623062eaf616d8e4
stakiran <stakiran@users.noreply.github.com>
2020-12-21 20:32:10 +0900

    default branch 問題ではなさそうなので戻す

--- main.tf
+++ main.tf
@@ -6,25 +6,14 @@ resource "github_repository" "repo" {
   name        = "test-from-terraform"
   description = "Terraform からつくってみたものです"
   visibility  = "public"
-  auto_init   = true
-}
-
-resource "github_branch_default" "repo" {
-  repository = github_repository.repo.name
-  branch     = "master"
 }
 
 output "reponame" {
   value = github_repository.repo.name
 }
 
-output "branch" {
-  value = github_branch_default.repo
-}
-
 resource "github_repository_file" "readme" {
   repository = github_repository.repo.name
-  branch     = github_branch_default.repo.branch
   file       = "README.md"
   content    = "# これは Terraform から作成したものです"
 }
